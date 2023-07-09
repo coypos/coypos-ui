e0fb21a9577c7b1368434611554d38cedeb8a337
waveoffire <waveoffire98@gmail.com>
2023-07-09 20:47:40 +0200

    added api request feature

--- src/components/HelloComponent.vue
+++ src/components/HelloComponent.vue
@@ -1,23 +1,41 @@
 <template>
   <div class="hello">
     <h1>{{ $t("open") }}</h1>
+
+    <button @click="ktojestnajlepszy">Kto jest fajny</button>
+    {{ kto }}
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import { useI18n } from "vue-i18n";
-
+import type { AxiosInstance } from "axios";
+declare module "@vue/runtime-core" {
+  interface ComponentCustomProperties {
+    $axios: AxiosInstance;
+    catTags: string[];
+  }
+}
 export default defineComponent({
   name: "HelloComponent",
+  data() {
+    return {
+      kto: "",
+    };
+  },
   setup() {
     const { t } = useI18n({
       inheritLocale: true,
       useScope: "local",
     });
 
-    // Something todo ..
-
     return { t };
+  },
+  methods: {
+    async ktojestnajlepszy() {
+      this.kto = await this.$axios.get("/ktofajnyjest");
+      console.log(this.kto);
+    },
   },
 });
 </script>
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import i18n from "./i18n";
-
-createApp(App).use(i18n).use(store).use(router).mount("#app");
+import axios from "./axios";
+console.log(process.env.VUE_APP_API_URL);
+createApp(App)
+  .use(i18n)
+  .use(store)
+  .use(router)
+  .use(axios, {
+    baseUrl: process.env.VUE_APP_API_URL || "http://localhost:5016",
+  })
+  .mount("#app");
