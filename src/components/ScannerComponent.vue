8a3b56b02cef33c9a86d70f3a309a652c9131420
waveoffire <waveoffire98@gmail.com>
2023-08-23 22:06:57 +0200

    add ScannerComponent, that work in CartView, COYPOS-34

---
+++ src/components/ScannerComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="scanner">
+    na ten moment dziala wpisywanie numerow i zatwierdzenie enterem
+    <h1>{{ scanned }}</h1>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "ScannerComponent",
+  data() {
+    return {};
+  },
+  setup() {
+    let scanned = ref<string>("");
+    return { scanned };
+  },
+  methods: {
+    scannerEvent() {
+      window.addEventListener("input", this.handleBarcodeInput);
+      window.addEventListener("keydown", this.handleBarcodeInput);
+    },
+    handleBarcodeInput(event: Event) {
+      const inputValue = (event as KeyboardEvent).key;
+      if (inputValue && !isNaN(parseInt(inputValue))) {
+        this.scanned += inputValue;
+      } else if (inputValue == "Enter") {
+        console.log(`wyszukaj produkt o numerze ${this.scanned}`);
+        this.scanned = "";
+      }
+    },
+    removeScannerEvent() {
+      window.removeEventListener("keydown", this.handleBarcodeInput);
+      window.removeEventListener("input", this.handleBarcodeInput);
+    },
+  },
+  mounted() {
+    this.scannerEvent();
+  },
+  unmounted() {
+    this.removeScannerEvent();
+  },
+});
+</script>
--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -3,6 +3,7 @@
     <h1>This is an Cart page</h1>
     <cart-component></cart-component>
     <pay-component></pay-component>
+    <scanner-component></scanner-component>
   </div>
 </template>
 <script lang="ts">
@@ -10,11 +11,12 @@ import { defineComponent } from "vue";
 
 import CartComponent from "@/components/CartComponent.vue";
 import PayComponent from "@/components/PayComponent.vue";
+import ScannerComponent from "@/components/ScannerComponent.vue";
 
 import { useI18n } from "vue-i18n";
 export default defineComponent({
   name: "ThingsView",
-  components: { CartComponent, PayComponent },
+  components: { CartComponent, PayComponent, ScannerComponent },
   setup() {
     const { t } = useI18n({
       inheritLocale: true,
