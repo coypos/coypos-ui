69a8d1aec9e9b50c3fe36ad9eb80b886b7f25bf6
waveoffire <waveoffire98@gmail.com>
2023-09-14 20:51:22 +0200

    fix hosts error

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  devServer: {
+    allowedHosts: "all",
+  },
   pluginOptions: {
     i18n: {
       locale: "pl",
