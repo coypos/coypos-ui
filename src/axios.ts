f5a35478dbc145ae217ecb05a44111111466777e
waveoffire <waveoffire98@gmail.com>
2023-10-18 19:30:02 +0200

    caching system

--- src/axios.ts
+++ src/axios.ts
@@ -1,6 +1,6 @@
-import axios from "axios";
+import Axios from "axios";
 import type { App } from "vue";
-
+import { setupCache, buildWebStorage } from "axios-cache-interceptor";
 interface AxiosOptions {
   baseUrl?: string;
   token?: string;
@@ -8,13 +8,16 @@ interface AxiosOptions {
 
 export default {
   install: (app: App, options: AxiosOptions) => {
-    app.config.globalProperties.$axios = axios.create({
-      baseURL: options.baseUrl,
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: options.token ? `Bearer ${options.token}` : "",
-        XApiKey: process.env.VUE_APP_TOKEN,
-      },
-    });
+    app.config.globalProperties.$axios = setupCache(
+      Axios.create({
+        baseURL: options.baseUrl,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: options.token ? `Bearer ${options.token}` : "",
+          XApiKey: process.env.VUE_APP_TOKEN,
+        },
+      }),
+      { storage: buildWebStorage(localStorage, "axios-cache:") }
+    );
   },
 };
